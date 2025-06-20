5bd5bef89c143577087010b824d74daef1b7680f
rmroot <rootrm@ornl.gov>
2025-06-20 10:36:01 -0500

    Add forwarding to new industrial resources page

--- docs/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['fast_20c_2b_2b_20csv_20parser',['Fast C++ CSV Parser',['../index.html',1,'']]]
-];
--- docs/html/search/variables_6.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['motorcurrent',['motorCurrent',['../dc/df8/struct_p_s_a_t_result_1_1_output.html#a90611798efa48c24155f6c957683d3a8',1,'PSATResult::Output']]],
-  ['motorefficiency',['motorEfficiency',['../dc/df8/struct_p_s_a_t_result_1_1_output.html#ad1beb6e9a1fc9bc21c50eb42ae598589',1,'PSATResult::Output']]],
-  ['motorpower',['motorPower',['../dc/df8/struct_p_s_a_t_result_1_1_output.html#a531700e73ed9cc95d1dd4b2d58030ffb',1,'PSATResult::Output']]],
-  ['motorpowerfactor',['motorPowerFactor',['../dc/df8/struct_p_s_a_t_result_1_1_output.html#a4e99d794b66677cc0329f47a100d251a',1,'PSATResult::Output']]],
-  ['motorratedpower',['motorRatedPower',['../dc/df8/struct_p_s_a_t_result_1_1_output.html#a72f4e093c7cd3ee1ea943cebd79c2d7a',1,'PSATResult::Output']]],
-  ['motorshaftpower',['motorShaftPower',['../dc/df8/struct_p_s_a_t_result_1_1_output.html#a813a97afcb5ea5465d8b733eaa4f834c',1,'PSATResult::Output']]]
-];
